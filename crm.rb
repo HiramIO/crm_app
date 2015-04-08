ae3760c1bea11c02bd2a792df94f8aad7065b126
Harrison Taylor <h.taylor@project89.org>
2015-04-08 16:02:02 -0400

    Finished add contacts html

--- crm.rb
+++ crm.rb
@@ -3,21 +3,18 @@ require 'sinatra'
 require_relative 'contact'
 require_relative 'rolodex'
 
+$rolodex= Rolodex.new
+
 get '/' do
   @crm_app_name = "My CRM"
   erb :index
 end
 
 get '/contacts' do
-  @contacts = []
-  @contacts << Contact.new("Yehuda", "Katz", "yehuda@example.com", "Developer")
-  @contacts << Contact.new("Mark", "Zuckerberg", "mark@facebook.com", "CEO")
-  @contacts << Contact.new("Sergey", "Brin", "sergey@google.com", "Co-Founder")
-
   erb :contacts
 end
 
-get '/add' do
-  erb :add
+get '/contacts/new' do
+  erb :new_contact
 end
 
