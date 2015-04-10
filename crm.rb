6135189af098269bda050db8a7e173ad6c19070b
Harrison Taylor <h.taylor@project89.org>
2015-04-10 14:09:31 -0400

    Added show user deets functionality

--- crm.rb
+++ crm.rb
@@ -3,7 +3,10 @@ require 'sinatra'
 require_relative 'contact'
 require_relative 'rolodex'
 
-$rolodex= Rolodex.new
+@@rolodex= Rolodex.new
+@@rolodex.add_contact(Contact.new("Johnny", "Bravo", "johnny@bitmakerlabs.com", "Rockstar"))
+
+contact = @@rolodex.find(1000)
 
 get '/' do
   @crm_app_name = "My CRM"
@@ -18,9 +21,14 @@ get '/contacts/new' do
   erb :new_contact
 end
 
+get '/contacts/1000' do
+  @contact = @@rolodex.find(1000)
+  erb :contact_details
+end
+
 post '/contacts' do
   new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
-  $rolodex.add_contact(new_contact)
+  @@rolodex.add_contact(new_contact)
   redirect to('/contacts')
 end
 
